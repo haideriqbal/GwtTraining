d8b201a0f96b6eb252179652451938a207be266b
haideriqbal <haideriqbal1@hotmail.com>
2016-11-06 17:18:51 +0500

    Implemented the StockWatcher UI Interface

--- src/com/mySampleApplication/client/MySampleApplication.java
+++ src/com/mySampleApplication/client/MySampleApplication.java
@@ -12,45 +12,38 @@ import com.google.gwt.event.dom.client.ClickEvent;
  */
 public class MySampleApplication implements EntryPoint {
 
+    private VerticalPanel mainPanel = new VerticalPanel();
+    private HorizontalPanel addPanel = new HorizontalPanel();
+    private FlexTable stocksFlexTable = new FlexTable();
+    private TextBox newSymbolTextBox = new TextBox();
+    private Button addBtn = new Button("Add");
+    private Label timeStampLabel = new Label();
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Button button = new Button("Click me");
-        final Label label = new Label();
 
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    MySampleApplicationService.App.getInstance().getMessage("This is Haider Iqbal Reporting!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
-            }
-        });
+        //setting up the table header
+        stocksFlexTable.setText(0,0,"Symbol");
+        stocksFlexTable.setText(0,1,"Price");
+        stocksFlexTable.setText(0,2,"Change");
+        stocksFlexTable.setText(0,3,"Remove");
 
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
+        //setting up the bottom textbox and button in Horizontal Layout
+        addPanel.add(newSymbolTextBox);
+        addPanel.add(addBtn);
+
+        //setting up the main vertical panel
+        mainPanel.add(stocksFlexTable);
+        mainPanel.add(addPanel);
+        mainPanel.add(timeStampLabel);
+
+
+        //associating the root panel with the main vertical panel
+        RootPanel.get("stockList").add(mainPanel);
+        newSymbolTextBox.setFocus(true);
+
     }
 
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
 
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
-    }
 }
